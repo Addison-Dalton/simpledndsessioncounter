3d45288d5be029da64d99bf20edcfebdced54192
Addison Dalton <zarch14@gmail.com>
2019-02-18 20:19:54 -0500

    added countdown feature. Added simple login feature to change selected day.

--- main.js
+++ main.js
@@ -1,53 +1,74 @@
 $(document).ready( () => {
 
     var url = 'https://api.myjson.com/bins/jpoqi';
-    var $dayNumberElem = $('#die-number');
-    var numberOfDays = 0;
-    var selectedDate = getSelectedDate();
-    console.log('data' + selectedDate);
     var dateDiff = 0;
+    var selectedDate;
+    var selectedDateJSON;
+    const password = 'dalton';
 
-    getNumberOfDays();
-    setDate();
+    $datePicker = $('#datepicker');
+    $setDatePasswordForm = $('#setDatePasswordForm');
+    $passwordInput = $('#passwordInput');
+    $passwordIncorrect = $('#passwordIncorrect');
+    $dayNumberElem = $('#die-number');
+
+    $setDatePasswordForm.on('submit', function(e) {
+        e.preventDefault();
+        checkPassword($passwordInput.val());
+    });
+
+    $('#closeSetDateModal').on('click', function() {
+        $datePicker.addClass('hidden');
+    });
+
+    
 
     var datePicker = new Pikaday({ 
         field: document.getElementById('datepicker'),
+        defaultDate: new Date(selectedDateJSON),
+        setDefaultDate: true,
         onSelect: function() {
             updateSelectedDate();
-            numberOfDays = getNumberOfDays();
+            setNumberOfDays();
 
             // Updates the die number
             setDate();
         }
     });
 
-    function getNumberOfDays() {
+    $.get(url, function(data, textStatus, jqXHR) {
+        selectedDateJSON = data.date;
+        selectedDate = new Date(selectedDateJSON);
+        setNumberOfDays();
+        setDate();
+    });
+
+    function checkPassword(enteredPassword) {
+        $passwordIncorrect.addClass('hidden');
+        if (enteredPassword === password) {
+            $datePicker.toggleClass('hidden');
+            $setDatePasswordForm.toggleClass('hidden');
+        } else {
+            $passwordIncorrect.removeClass('hidden');
+        }
+    }
+
+    function setNumberOfDays() {
         var oneDay = 24*60*60*1000;
         var today = new Date();
-
         // if the current date is the same as the selected D&D date, set the dateDiff to 0
         if (today.getDate() === selectedDate.getDate() && today.getMonth() === selectedDate.getMonth() && today.getFullYear() === selectedDate.getFullYear()) {
             dateDiff = 0;
         } else {
             dateDiff = Math.round(Math.abs((today.getTime() - selectedDate.getTime()) / (oneDay)) + 1);
         }
-
-        return dateDiff;
     }
 
     function setDate() {
         $($dayNumberElem).text(dateDiff);
     }
 
-    function getSelectedDate() {
-        $.get(url, function(data, textStatus, jqXHR) {
-            console.log(data);
-            console.log(data.date);
-            return data.date;
-        });
-    }
-
-    function updateSelectedDate(date) {
+    function updateSelectedDate() {
         selectedDate = datePicker.getDate();
         var dataurl = '{"date":' + '"' + selectedDate.toJSON() + '"' + '}';
         $.ajax({
